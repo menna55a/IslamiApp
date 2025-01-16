26938c5da84027c0ce5cbf57353f45a0d670e8f7
DELL <mennaahmed20012015@gamil.com>
2025-01-16 20:30:39 +0200

    Initial Commit

---
+++ app/src/main/java/com/route/islamiapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.route.islamiapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    // 1- Version Control System (Git)
+    // 2- Islamic App
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+    }
+
+}
