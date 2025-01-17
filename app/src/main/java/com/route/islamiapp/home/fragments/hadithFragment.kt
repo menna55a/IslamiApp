447fa0df75c4bf2d26f37df9535c63775a8a2e5a
DELL <mennaahmed20012015@gamil.com>
2025-01-17 02:19:34 +0200

    Initial Commit

---
+++ app/src/main/java/com/route/islamiapp/home/fragments/hadithFragment.kt
@@ -0,0 +1,23 @@
+package com.route.islamiapp.home.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.route.islamiapp.databinding.FragmentHadithBinding
+import com.route.islamiapp.databinding.FragmentQuranBinding
+
+class hadithFragment : Fragment() {
+    lateinit var binding: FragmentHadithBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHadithBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- app/src/main/java/com/route/islamiapp/splash/IslamiSplashActivity.kt
+++ app/src/main/java/com/route/islamiapp/splash/IslamiSplashActivity.kt
@@ -1,9 +1,13 @@
 package com.route.islamiapp.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.route.islamiapp.R
+import com.route.islamiapp.home.HomeActivity
 
 class IslamiSplashActivity : AppCompatActivity() {
     // 1- Version Control System (Git)
@@ -13,6 +17,16 @@ class IslamiSplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_islami)
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                navigationScreen()
+            },2000)
+
     }
 
+    private fun navigationScreen(){
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
